d1e48abe8551b2cd62a5b8119a92c04e6fd7ce23
huannguyen2114 <huan.nguyen2114@gmail.com>
2024-02-27 07:03:50 +0700

    modifying routers

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -9,30 +9,27 @@ import (
 	"strconv"
 
 	"github.com/huannguyen2114/golang-project/snippetbox/internal/models"
+	"github.com/julienschmidt/httprouter"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		app.notFound(w)
-		return
-	}
-
 	snippets, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, err)
 		return
 	}
-	// Call the newTemplateData() helper to get a tmplateData struct containing
-	// the 'default' data
 	data := app.newTemplateData(r)
 	data.Snippets = snippets
 
-	// Pass the datat o the render() helper as normal.
 	app.render(w, http.StatusOK, "home.tmpl.html", data)
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	// When httprouter is parsing a request, the vlaues of any named parameters will be stored in tehe request context.
+	params := httprouter.ParamsFromContext(r.Context())
+
+	// Use ByName() method to get the value of the "id" named parameter from the slice and validate it as normal
+	id, err := strconv.Atoi(params.ByName("id"))
 	if err != nil || id < 1 {
 		app.notFound(w)
 		return
@@ -56,22 +53,18 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", http.MethodPost)
-		app.clientError(w, http.StatusMethodNotAllowed)
-		return
-	}
+	w.Write([]byte("Display the form for creating a new snippet...."))
+}
+
+func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 	title := "0 snail"
 	content := "0 snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n- Kobayashi Issa"
 	expires := 7
-
-	// Pass the data to the SnippetModel.Insert() method, receiving the Id of the new record back
 	id, err := app.snippets.Insert(title, content, expires)
 	if err != nil {
-
 		app.serverError(w, err)
 		return
 	}
-	// redirect the user to the relevant page for the snippet
-	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
+
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
